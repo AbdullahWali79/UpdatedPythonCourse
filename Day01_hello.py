5f3883bef3b15822a6ffa0b5f2c1b527a077199a
aoextgarhamoresb-code <aoextgarhamore.sb@gmail.com>
2026-09-15 17:23:56 +0500

    Create Day01_hello.py

---
+++ Day01_hello.py
@@ -0,0 +1,9 @@
+# Step 1: Store the two numbers in variables
+number1 = 5
+number2 = 10
+
+# Step 2: Add the numbers together
+sum_result = number1 + number2
+
+# Step 3: Print the answer
+print("The sum is:", sum_result)
